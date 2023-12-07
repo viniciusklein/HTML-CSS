df2e99c59c0070ec65489b17187f13535800bd2c
viniciusklein <vini020306@gmail.com>
2023-12-07 11:39:55 -0300

    att

---
+++ Testes/Teste Chat gpt/script.js
@@ -0,0 +1,5 @@
+// Este é um exemplo simples de JavaScript para interação com a página
+document.addEventListener('DOMContentLoaded', function() {
+    // Você pode adicionar código JavaScript aqui para interagir com a página
+    console.log('O script foi carregado!');
+});
